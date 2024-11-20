0ae542cde2d831401b0b3e9eede172a1c34349eb
Rod Christiansen <rchristiansen@ecuad.ca>
2024-11-20 12:13:20 -0800

    Initial Commit

---
+++ AirName/AirNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirNameApp.swift
+//  AirName
+//
+//  Created by Rod Christiansen on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AirNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirNameTests/AirNameTests.swift
@@ -0,0 +1,16 @@
+//
+//  AirNameTests.swift
+//  AirNameTests
+//
+//  Created by Rod Christiansen on 11/20/24.
+//
+
+import Testing
+
+struct AirNameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
